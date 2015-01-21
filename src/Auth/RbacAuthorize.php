dd9e6abad3a49c680ac3ef74e47e86a09b66f0b1
Jad Bitar <jadbitar@mac.com>
2015-01-21 17:03:51 -0500

    Add more permission matches

--- src/Auth/RbacAuthorize.php
+++ src/Auth/RbacAuthorize.php
@@ -5,6 +5,7 @@ namespace CrudUsers\Auth;
 use Cake\Auth\BaseAuthorize;
 use Cake\Network\Request;
 use Cake\ORM\TableRegistry;
+use Cake\Routing\Exception\MissingRouteException;
 use Cake\Routing\Router;
 
 class RbacAuthorize extends BaseAuthorize
@@ -21,8 +22,43 @@ class RbacAuthorize extends BaseAuthorize
                 'associated' => [$this->config('groupModel')]
             ]);
 
-        $route = Router::url(array_filter($request->params));
-        $permission = preg_replace('~^/~', '', strtolower($route));
-        return $user->hasAccess($permission);
+        $params = array_filter($request->params, function($item) {
+            return !empty($item) && is_string($item);
+        });
+        $permissions = [];
+
+        $route = $this->_getRouteMatch($params);
+        $permissions[] = preg_replace('~^/~', '', strtolower($route));
+
+        while (count($params)) {
+            $permissions[] = strtolower(implode('/', $params));
+            array_pop($params);
+        }
+
+        return $user->hasAnyAccess(array_unique($permissions));
+    }
+
+    protected function _getRouteMatch(array $params)
+    {
+        try {
+            $route = Router::url($params);
+        } catch (MissingRouteException $e) {
+            if (!empty($params['_method'])) {
+                throw new MissingRouteException($e->getMessage());
+            }
+        }
+
+        foreach (['DELETE', 'PUT', 'POST', 'GET'] as $method) {
+            try {
+                $route = Router::url($params + ['_method' => $method]);
+                break;
+            } catch (MissingRouteException $e) {}
+        }
+
+        if (empty($route)) {
+            throw new MissingRouteException($e->getMessage());
+        }
+
+        return $route;
     }
 }
